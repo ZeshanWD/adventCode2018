1f52bfe83c7963abf579166288fe1317515d854c
ZeshanWD <zeeshan9choudhary@gmail.com>
2018-12-06 16:28:14 +0100

    solved day 3 part1

--- 03/solutionPart2.go
+++ 03/solutionPart2.go
@@ -29,41 +29,61 @@ func main() {
 	}
 
 	var claimsArr []claim
+	var cleanArr = make(map[string]int)
 	var mapa [1000][1000]int
 	for i := 0; i < len(data); i++ {
-		claimsArr = append(claimsArr, parseLine(data[i]))
+		lineaParseada := parseLine(data[i])
+		claimsArr = append(claimsArr, lineaParseada)
 	}
 
 	for _, rect := range claimsArr {
 		for i := rect.x; i < rect.x+rect.width; i++ {
 			for j := rect.y; j < rect.y+rect.heigth; j++ {
 				mapa[i][j]++
+				cleanArr[rect.id] = mapa[i][j]
 			}
 		}
 	}
 
-	conflicts := countConflicts(mapa)
+	// conflicts := countConflicts(mapa)
 
-	fmt.Println("conflicts", conflicts)
+	fmt.Println("clean claims", getCleanClaim(cleanArr))
 }
 
-func countConflicts(mapa [1000][1000]int) int {
-	conflicts := 0
-	for _, arr := range mapa {
-		for _, value := range arr {
-			if value > 1 {
-				conflicts++
-			}
+func getCleanClaim(arr map[string]int) string {
+	var found string
+	for key, _ := range arr {
+		if arr[key] == 1 {
+			found = key
+			fmt.Println("Found", found)
+			os.Exit(0)
 		}
 	}
 
-	return conflicts
+	return found
+}
+
+// func countConflicts(mapa [1000][1000]int) int {
+// 	conflicts := 0
+// 	for _, arr := range mapa {
+// 		for _, value := range arr {
+// 			if value > 1 {
+// 				conflicts++
+// 			}
+// 		}
+// 	}
+
+// 	return conflicts
+// }
+
+func removeIndex(s []int, index int) []int {
+	return append(s[:index], s[index+1:]...)
 }
 
 func parseLine(line string) claim {
 	item := strings.FieldsFunc(line, func(r rune) bool {
 		switch string(r) {
-		case "@", ",", "x", ":":
+		case "@", ",", "x", ":", "#":
 			return true
 		default:
 			return false
